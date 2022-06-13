dbf53ff9afa4aaf38d1caa7f6509a0f66ea6077e
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-12 21:41:11 -0300

    4 criado Taskresources

---
+++ src/resources/TasksResource.js
@@ -0,0 +1,91 @@
+import AdminJS from 'adminjs';
+
+import Task from '../models/task';
+
+export default {
+    resource: Task,
+    options: {
+        parent: {
+            icon: 'Task'
+        },
+        actions: {
+            resetPassword: {
+                actionType: 'record',
+                icon: 'Password',
+                handler: async (request, response, context) => {
+                    return {
+                        record: context.record.toJson(),
+                    };
+                },
+            },
+        },
+        properties: {
+            id: {
+                position: 1,
+            },
+            title: {
+                position: 2,
+                isRequired: true,
+            },
+            description: {
+                position: 3,
+                isVisible : { list: false, filter: false, show: true, edit:true},
+                type: 'richtext',
+/*                 props: {
+                    quill: {
+                        modules: {
+                            toolbar: [
+                                ['bold', 'italic'],
+                                ['link', 'image']
+                            ],
+                        },
+                    },
+                }, */
+            },
+            due_date: {
+                position: 4,
+            },
+            effort: {
+                position: 5,
+            },
+            order: {
+                position: 6,
+            },
+            status: {
+                position: 7,
+                isRequired: true,
+                availableValues: [
+                    { value: 'backlog', label: 'backlog' },
+                    { value: 'doing', label: 'Em execuçao'},
+                    { value: 'done', label: 'Pronto'},
+                    { value: 'approved', label: 'Aprovado'},
+                    { value: 'rejected', label: 'Rejeitado'},
+                ]
+            },
+            projectId: {
+                position: 8,
+                isRequired: true,
+                isVisible: {list: false, filter: true, show: true, edit: true }
+            },
+            userId: {
+                position: 9,
+                isRequired: true
+            },
+            createdAt: {
+                position: 10,
+                isVisible: { list: true, filter: true, show: true, edit: false }
+            },
+            updatedAt: {
+                position: 11,
+                isVisible: { list: false, filter: true, show: true, edit: false }
+            },
+            user_id: {
+                isVisible: false,
+            },
+            project_Id: {
+                isVisible: false,
+            }
+        }
+    }
+
+}
