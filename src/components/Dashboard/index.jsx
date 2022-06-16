59aca161da553aef5f41374ea1595148329534c7
devLucasMoraes <devlucasmoraes@gmai.com>
2022-06-15 21:31:32 -0300

    box com os translate

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react'
 
-import { ApiClient } from 'adminjs'
+import { ApiClient, useTranslation } from 'adminjs'
 import { Box, H2, Text} from '@adminjs/design-system'
 
 const api = new ApiClient()
 
 const Dashboard = () => {
+    const { translateMessage } = useTranslation();
     const [data, setData] = useState({})
 
     useEffect(() => {
@@ -18,8 +19,8 @@ const Dashboard = () => {
             <Box position="relative" overflow="hidden">
                 <Box bg="grey20" height={284} py={74} px={['defalt', 'lg', 250]}>
                     <Text textAlign="center" color="primary100">
-                        <H2>Titulo</H2>
-                        <text opacity="0.8">Subtitulo</text>
+                        <H2>{translateMessage("dashboardTitle")}</H2>
+                        <text opacity="0.8">{translateMessage("dashboardSubTitle")}</text>
                     </Text>
                 </Box>
             </Box>
